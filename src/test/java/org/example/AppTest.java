801ea66cf2c72ddd7765b70206638d862151c6c3
Roger Ruiz <roger.ruiz@oktana.com>
2022-06-09 16:21:37 -0400

    anything you want to put here

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,12 +1,34 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
 
 public class AppTest {
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue( true );
+    public void firstTest() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://oktana-1fb-dev-ed.my.salesforce.com/");
+
+        WebElement usernameField = driver.findElement(By.xpath("//input[@id='username']"));
+        WebElement passwordField = driver.findElement(By.xpath("//input[@id='password']"));
+        WebElement loginButton = driver.findElement(By.xpath("//input[@id='Login']"));
+
+        usernameField.sendKeys("developers.lwc@oktana.com");
+        passwordField.sendKeys("dev.lwc2023");
+
+        loginButton.click();
+
+        String pageTitle = driver.getTitle();
+
+        Assert.assertEquals(pageTitle, "Home | Salesforce", "Error message");
+
+
     }
 }
